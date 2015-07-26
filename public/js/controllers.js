8b067e46b9f81afb497c958c1fc7eeb807385763
Luke Kysow <lkysow@gmail.com>
2015-07-26 11:51:50 -0700

    Use new /api endpoint

--- public/js/controllers.js
+++ public/js/controllers.js
@@ -5,7 +5,7 @@ var cantechControllers = angular.module('cantechControllers', []);
 
 cantechControllers.controller('NavCtrl', ['$scope', '$http',
     function ($scope, $http) {
-        $http.get('sessions').success(function (data) {
+        $http.get('/api/sessions').success(function (data) {
             $scope.sessions = data.results;
         });
     }]);
