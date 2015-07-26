408a9267a366e6fae739fd00f9d6986a06671584
Luke Kysow <lkysow@gmail.com>
2015-07-26 11:47:14 -0700

    Add partial for main view

--- public/js/controllers.js
+++ public/js/controllers.js
@@ -9,3 +9,13 @@ cantechControllers.controller('NavCtrl', ['$scope', '$http',
             $scope.sessions = data.results;
         });
     }]);
+
+cantechControllers.controller('ClassCtrl', ['$scope', '$http', '$routeParams',
+    function ($scope, $http, $routeParams) {
+        //$http.get('sessions').success(function (data) {
+        //    $scope.sessions = data.results;
+        //});
+        if ($routeParams.id) {
+            $scope.class = 'hi';
+        }
+    }]);
