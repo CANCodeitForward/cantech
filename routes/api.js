b40ed52f59f67e5b40dee8e63cb8b01e1d3f0402
Kenny Teng <provpup@users.noreply.github.com>
2015-07-26 17:41:42 +0000

    Add route for class worker registrations

--- routes/api.js
+++ routes/api.js
@@ -18,8 +18,46 @@ var connection = mysql.createConnection({
     password : config.MYSQL_PASSWORD
 });
 
+// TODO: refactor class_worker_registrations and class_participant_registrations functions into closure
 
-exports.class_registrations = function(req, res){
+exports.class_worker_registrations = function(req, res){
+
+  var classid = req.params.classid;
+  console.log('classid: ' + classid);
+
+  if(!classid){
+    res.json({ "message": "classid cannot be empty!", "status": "error"}, 404);
+  }
+
+  // This SQL query returns all the workers who are registered for a particular class
+  var sql = 'select session.name, class.id as class_id, class.session_id, worker_registration.worker_id, worker.first_name, worker.last_name, worker.email_address from (candb.worker_registration inner join (candb.class inner join candb.session on class.session_id = session.id) on worker_registration.session_id = class.session_id) inner join candb.worker on worker_registration.worker_id = worker.id where class.id = ' + classid +' ORDER BY worker.last_name;'
+  var query = connection.query(sql, function(err, result) {   
+    if (err) {
+      console.log(err);
+      res.json({"message": "Something went wrong!", "results": null}, 500); 
+    } else {
+      if (result.length === 0) {
+        res.json({"message": "No registrations found", "results": null}, 404); 
+      } else {
+        console.log(result);
+        // This SQL query returns all the workers who have signed in and signed out
+        sql = 'select worker_attendance.worker_id, worker_attendance.time, worker_attendance.type from candb.worker_attendance where worker_attendance.class_id = ' + classid + ' ORDER BY worker_id;';
+        var query2 = connection.query(sql, function(err, result2) {
+          // We manually stitch the two results together because the two existing SQL queries are complicated enough
+          result.forEach(function (currentValue) {
+            currentValue.attendance = result2.filter(function matchingWorkers(filterValue) {
+             return filterValue.worker_id == currentValue.worker_id;
+            });
+          });
+          // Return to view
+          res.json({"message": "Successfully gathered registrations and attendance", "results": result}, 200);
+        });
+      }
+    }
+  });
+};
+
+exports.class_participant_registrations = function(req, res){
 
   var classid = req.params.classid;
   console.log('classid: ' + classid);
