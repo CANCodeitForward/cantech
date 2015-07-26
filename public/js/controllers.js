780670bc9b6eb7a736703b6ae36345096cf0b61e
Luke Kysow <luke.kysow@hootsuite.com>
2015-07-26 11:56:02 -0700

    Move footer to partial

--- public/js/controllers.js
+++ public/js/controllers.js
@@ -19,3 +19,13 @@ cantechControllers.controller('ClassCtrl', ['$scope', '$http', '$routeParams',
             $scope.class = 'hi';
         }
     }]);
+
+cantechControllers.controller('FooterCtrl', ['$scope', '$http', '$routeParams',
+    function ($scope, $http, $routeParams) {
+        //$http.get('sessions').success(function (data) {
+        //    $scope.sessions = data.results;
+        //});
+        if ($routeParams.id) {
+            $scope.class = 'hi';
+        }
+    }]);
