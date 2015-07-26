f3afd3c620e30b0355bf9a1c71f8f2ae62efb9d7
Anubhav Mishra <anubhav.mishra@hootsuite.com>
2015-07-25 19:31:49 -0700

    Login with db

--- routes/index.js
+++ routes/index.js
@@ -4,19 +4,19 @@
 
 var url = require('url');
 var path = require('path');
-var request = require('request');
 var mysql = require('mysql');
 var sanitizer = require('sanitizer');
 var ejs = require('ejs');
 var fs = require('fs');
 var http = require('http');
+var config = require('../config.json');
 
 // MySQL Config
-/**var connection = mysql.createConnection({
+var connection = mysql.createConnection({
     host     : config.MYSQL_HOST,
     user     : config.MYSQL_USER,
     password : config.MYSQL_PASSWORD
-});**/
+});
 
 exports.index = function(req, res){ 
     res.send("HEY!");
@@ -29,9 +29,7 @@ exports.login = function(req, res){
 exports.login_user = function(req, res){
 
     var email = sanitizer.sanitize(req.body.email);
-    console.log('email: ' + email);
     var password = sanitizer.sanitize(req.body.password);
-    console.log('password: ' + password);
 
     if(!email){
         res.json({ "message": "Email cannot be empty!", "status": "error"}, 500);
@@ -40,9 +38,40 @@ exports.login_user = function(req, res){
         res.json({ "message": "Password cannot be empty!", "status": "error"}, 500);
     }
 
-    // TODO: Authenticate with database
-
-    req.session.email = email;
-
-    res.redirect('/dashboard');
+    // Create mysql query 
+    var sql = 'SELECT * FROM candb.user WHERE email = ' + connection.escape(email) + ' AND password = ' + connection.escape(password); 
+    // Get user id if it is a valid user
+    var query = connection.query(sql, function(err, result) {   
+        if (err){ 
+            console.log(err);
+            res.json({"message": "Something went wrong!", valid: false}); 
+        }else{
+         if (result.length === 0) {
+            res.json({"message": "User doesn't exists!", valid: false}); 
+         } else {
+            // Set user id
+            var user_id = result[0].id;
+            // Worker id in Session
+            req.session.worker_id = result[0].worker_id;
+            // Save User ID in Session
+            req.session.user_id = user_id;
+            // Save User Email in Session 
+            req.session.email = email;
+            // Return to view
+            res.json({"message": "Successfully logged in!", valid: true}, 200);
+            }
+        }
+    });
 };
+
+exports.dashboard = function(req, res){ 
+
+    if(!req.session.email){
+        res.redirect('/');
+    }else{
+
+    
+    }
+};
+
+
---
+++ schema/candb_2015-07-25.sql
@@ -0,0 +1,211 @@
+# ************************************************************
+# Sequel Pro SQL dump
+# Version 4096
+#
+# http://www.sequelpro.com/
+# http://code.google.com/p/sequel-pro/
+#
+# Host: 127.0.0.1 (MySQL 5.6.19-0ubuntu0.14.04.1)
+# Database: candb
+# Generation Time: 2015-07-26 02:25:46 +0000
+# ************************************************************
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+
+# Dump of table class
+# ------------------------------------------------------------
+
+DROP TABLE IF EXISTS `class`;
+
+CREATE TABLE `class` (
+  `id` int(11) NOT NULL,
+  `session_id` int(11) DEFAULT NULL,
+  `date` datetime DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `session_id` (`session_id`),
+  CONSTRAINT `class_ibfk_1` FOREIGN KEY (`session_id`) REFERENCES `session` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+
+
+# Dump of table participant
+# ------------------------------------------------------------
+
+DROP TABLE IF EXISTS `participant`;
+
+CREATE TABLE `participant` (
+  `id` int(11) NOT NULL,
+  `email_address` varchar(64) DEFAULT NULL,
+  `gender` varchar(8) DEFAULT NULL,
+  `first_name` varchar(32) DEFAULT NULL,
+  `last_name` varchar(32) DEFAULT NULL,
+  `phone_id` varchar(32) DEFAULT NULL,
+  `emergency_contact1_id` int(11) DEFAULT NULL,
+  `emergency_contact2_id` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+
+
+# Dump of table participant_attendance
+# ------------------------------------------------------------
+
+DROP TABLE IF EXISTS `participant_attendance`;
+
+CREATE TABLE `participant_attendance` (
+  `id` int(11) NOT NULL,
+  `class_id` int(11) DEFAULT NULL,
+  `participant_id` int(11) DEFAULT NULL,
+  `time` datetime DEFAULT NULL,
+  `type` tinyint(1) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `class_id` (`class_id`),
+  KEY `participant_id` (`participant_id`),
+  CONSTRAINT `participant_attendance_ibfk_1` FOREIGN KEY (`class_id`) REFERENCES `class` (`id`),
+  CONSTRAINT `participant_attendance_ibfk_2` FOREIGN KEY (`participant_id`) REFERENCES `participant` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+
+
+# Dump of table participant_registration
+# ------------------------------------------------------------
+
+DROP TABLE IF EXISTS `participant_registration`;
+
+CREATE TABLE `participant_registration` (
+  `id` int(11) NOT NULL,
+  `session_id` int(11) DEFAULT NULL,
+  `participant_id` int(11) DEFAULT NULL,
+  `status` varchar(32) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `session_id` (`session_id`),
+  KEY `participant_id` (`participant_id`),
+  CONSTRAINT `participant_registration_ibfk_2` FOREIGN KEY (`participant_id`) REFERENCES `participant` (`id`),
+  CONSTRAINT `participant_registration_ibfk_1` FOREIGN KEY (`session_id`) REFERENCES `session` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+
+
+# Dump of table session
+# ------------------------------------------------------------
+
+DROP TABLE IF EXISTS `session`;
+
+CREATE TABLE `session` (
+  `id` int(11) NOT NULL,
+  `name` varchar(64) DEFAULT NULL,
+  `start_date` datetime DEFAULT NULL,
+  `end_date` datetime DEFAULT NULL,
+  `venue` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+
+
+# Dump of table user
+# ------------------------------------------------------------
+
+DROP TABLE IF EXISTS `user`;
+
+CREATE TABLE `user` (
+  `id` int(11) NOT NULL,
+  `email` varchar(255) DEFAULT NULL,
+  `password` varchar(255) DEFAULT NULL,
+  `type` varchar(255) DEFAULT NULL,
+  `worker_id` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `worker_id` (`worker_id`),
+  CONSTRAINT `user_ibfk_1` FOREIGN KEY (`worker_id`) REFERENCES `worker` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+LOCK TABLES `user` WRITE;
+/*!40000 ALTER TABLE `user` DISABLE KEYS */;
+
+INSERT INTO `user` (`id`, `email`, `password`, `type`, `worker_id`)
+VALUES
+	(0,'lkysow@gmail.com','password','staff',NULL);
+
+/*!40000 ALTER TABLE `user` ENABLE KEYS */;
+UNLOCK TABLES;
+
+
+# Dump of table worker
+# ------------------------------------------------------------
+
+DROP TABLE IF EXISTS `worker`;
+
+CREATE TABLE `worker` (
+  `id` int(11) NOT NULL,
+  `email_address` varchar(64) DEFAULT NULL,
+  `first_name` varchar(32) DEFAULT NULL,
+  `last_name` varchar(32) DEFAULT NULL,
+  `phone_number` varchar(32) DEFAULT NULL,
+  `type` varchar(32) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+LOCK TABLES `worker` WRITE;
+/*!40000 ALTER TABLE `worker` DISABLE KEYS */;
+
+INSERT INTO `worker` (`id`, `email_address`, `first_name`, `last_name`, `phone_number`, `type`)
+VALUES
+	(0,'john@john.com','john','john','12345689',NULL);
+
+/*!40000 ALTER TABLE `worker` ENABLE KEYS */;
+UNLOCK TABLES;
+
+
+# Dump of table worker_attendance
+# ------------------------------------------------------------
+
+DROP TABLE IF EXISTS `worker_attendance`;
+
+CREATE TABLE `worker_attendance` (
+  `id` int(11) NOT NULL,
+  `class_id` int(11) DEFAULT NULL,
+  `worker_id` int(11) DEFAULT NULL,
+  `time` datetime DEFAULT NULL,
+  `type` tinyint(1) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `class_id` (`class_id`),
+  KEY `worker_id` (`worker_id`),
+  CONSTRAINT `worker_attendance_ibfk_1` FOREIGN KEY (`class_id`) REFERENCES `class` (`id`),
+  CONSTRAINT `worker_attendance_ibfk_2` FOREIGN KEY (`worker_id`) REFERENCES `worker` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+
+
+# Dump of table worker_registration
+# ------------------------------------------------------------
+
+DROP TABLE IF EXISTS `worker_registration`;
+
+CREATE TABLE `worker_registration` (
+  `id` int(11) NOT NULL,
+  `session_id` int(11) DEFAULT NULL,
+  `worker_id` int(11) DEFAULT NULL,
+  `status` varchar(32) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `session_id` (`session_id`),
+  KEY `worker_id` (`worker_id`),
+  CONSTRAINT `worker_registration_ibfk_2` FOREIGN KEY (`worker_id`) REFERENCES `worker` (`id`),
+  CONSTRAINT `worker_registration_ibfk_1` FOREIGN KEY (`session_id`) REFERENCES `session` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+
+
+
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
